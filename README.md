0fa0ad71de297610d18eb9b9c4e0155397fa25bb
syeds <syedsalahuddin666@gmail.com>
2023-11-30 17:34:27 +0530

    mentioned some transformation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lending_Club_Project
+# by december i should able to complete lending club project with azure cloud
---
+++ Transformation.py
@@ -0,0 +1,8 @@
+# in spark so far i have revised some of the transformation
+#like 
+# map
+# filter,
+# reduceByKey
+# groupByKey
+# sortBy
+# sortByKey
